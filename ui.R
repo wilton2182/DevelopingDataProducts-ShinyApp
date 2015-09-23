cd7c0d0d4e462cf5a8575a151e5ed3d0d0fb3c42
wilton2182 <donwilton2182@gmail.com>
2015-09-23 07:08:41 -0400

    Minor cleanup and changed links to open to new tab.

--- ui.R
+++ ui.R
@@ -53,14 +53,14 @@ shinyUI(fluidPage(
                  verbatimTextOutput("lmsummry")),
         tabPanel("Residual Analysis Plots",
                  plotOutput("residplots"),
-                 h5(a("plot.lm() Help", href="https://stat.ethz.ch/R-manual/R-patched/library/stats/html/plot.lm.html"))
+                 h5(a("plot.lm() Help", href="https://stat.ethz.ch/R-manual/R-patched/library/stats/html/plot.lm.html", target="_blank"))
                  ),
         tabPanel("Help",
                  h4("Purpose"),
                  p("mtcars_Explorer is an interactive Shiny app that allows a user", 
                    "to evaluate individual predictor variables from the",
-                   a("mtcars dataset", href="https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/mtcars.html"),
-                   "as predictors of the response variable mpg. The hotlink above is to the ",
+                   a("mtcars dataset", href="https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/mtcars.html", target="_blank"),
+                   "as predictors of the response variable mpg. The link above is to the ",
                    "description of the mtcars dataset which describes the variables it contains."
                  ),
                  h4("Controls"),
@@ -72,9 +72,12 @@ shinyUI(fluidPage(
                  ),
                  h4("Processing"),
                  p("If a transformation is selected, then it is applied to the selected predictor", 
-                   "variable in the formula passed to the lm() function.  If prediction intervals or", 
-                   "confidence intervals are selected then those predictions are calculated using",
-                   "the predict.lm() function."
+                   "variable in the formula passed to the", 
+                   a("lm()",href="https://stat.ethz.ch/R-manual/R-patched/library/stats/html/lm.html", target="_blank"),
+                   "function.  If prediction intervals or", 
+                   "confidence intervals are selected then those predictions are calculated using the",
+                   a("predict.lm()",href="https://stat.ethz.ch/R-manual/R-patched/library/stats/html/predict.lm.html", target="_blank"),
+                   "function."
                  ),
                  h4("Output Tabs on Main Panel"),
                  p(strong("Plot - "),"A scatter plot with the regression line calculated using linear",
@@ -85,15 +88,16 @@ shinyUI(fluidPage(
                    "The prediction interval calculated at the 95% confidence level",
                    "represents the certainty associated with the predictions of mpg given at each",
                    "value of the transformed predictor variable. Narrower bands represent higher certainty."),
-                 p(strong("lm() Summary - "),"The ouput of summary(lm()) for the formula constructed based on the",
-                   "selected predictor variable and transformation. This tab is intended for advanced users.",
-                   "The R function ",
-                   a("summary.lm()", href="https://stat.ethz.ch/R-manual/R-patched/library/stats/html/summary.lm.html"),
+                 p(strong("lm() Summary - "),"This tab is intended for advanced users.",
+                   "The ouput of summary(lm()) for the formula constructed based on the",
+                   "selected predictor variable and transformation. The R function ",
+                   a("summary.lm()", href="https://stat.ethz.ch/R-manual/R-patched/library/stats/html/summary.lm.html", target="_blank"),
                    "provides an explanation of the output."),
-                 p(strong("lm() Residual Analysis Plots - "),"The plots are generated using plot.lm() and are used",
-                   "to diagnose problems with the data or models. This tab is intended for advanced users.",
-                   "See help for the R function", a("plot.lm()",href="https://stat.ethz.ch/R-manual/R-patched/library/stats/html/plot.lm.html"),
-                   "for an explanation of the output.")
+                 p(strong("lm() Residual Analysis Plots - "),"This tab is intended for advanced users.",
+                   "The plots are generated using plot.lm() and are used to diagnose problems with", 
+                   "the data or models.",
+                   a("plot.lm()",href="https://stat.ethz.ch/R-manual/R-patched/library/stats/html/plot.lm.html", target="_blank"),
+                   "provides an explanation of the output.")
         )
       )
     )
